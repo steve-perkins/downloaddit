6aeed1aad58a1def9d8098e215e0c0ceebbe7ce1
Steve Perkins <steve@steveperkins.com>
2015-12-26 09:46:08 -0500

    Populates the data grid and other fields on the second tab with results from the first tab

--- DownloadditWPF/MainWindow.xaml.cs
+++ DownloadditWPF/MainWindow.xaml.cs
@@ -4,6 +4,7 @@
     using System.Collections.Generic;
     using System.Linq;
     using System.Windows;
+    using System.Windows.Controls;
 
     using DownloadditLib;
 
@@ -17,11 +18,21 @@
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Event handler for the "File->Exit" menu option.  Terminates the application.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void ExitMenuItem_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
         }
 
+        /// <summary>
+        /// Event handler for the "File->About" menu option.  Displays descriptive text about the application.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void AboutMenuItem_Click(object sender, RoutedEventArgs e)
         {
             System.Windows.MessageBox.Show(
@@ -32,9 +43,16 @@ https://github.com/steve-perkins/downloaddit"
     , "About");
         }
 
+        /// <summary>
+        /// Event handler for the "Find Images" button.  Fetches image URL's based on the field values in 
+        /// the first tab, and uses that information to set field values in the second tab.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void FindImagesButton_Click(object sender, RoutedEventArgs e)
         {
-            
+            MainTabControl.SelectedItem = DownloadImagesTabItem;
+
             // TODO:  Refactor, and make this asynchronous so it doesn't block the main UI thread
 
             // Get user input from XAML fields
@@ -48,21 +66,42 @@ https://github.com/steve-perkins/downloaddit"
             HashSet<string> imageUrls = new HashSet<string>();
             HashSet<string> albumUrls = new HashSet<string>();
             string nextPageCursor = "";
-            for (int index = 0; index < maxPages && nextPageCursor != null; index++)
+            int pageCount = 1;
+            for ( ; pageCount <= maxPages && nextPageCursor != null; pageCount++)
             {
-                Console.WriteLine("Processing page {0} for {1} {2}", index + 1, type.ToString().ToLower(), entity);
+                Console.WriteLine("Processing page {0} for {1} {2}", pageCount, type.ToString().ToLower(), entity);
                 Uri redditUrl = RedditUtils.BuildUrl(entity, type, "".Equals(nextPageCursor) ? null : nextPageCursor);
                 RedditUtils.ParsePage(redditUrl, ref imageUrls, ref albumUrls, out nextPageCursor);
                 if (nextPageCursor != null) Console.WriteLine("nextPageCursor == {0}", nextPageCursor);
             }
-            Console.WriteLine("Found {0} image URL's and {1} album URL's", imageUrls.Count, albumUrls.Count);
+            Console.WriteLine("Parsed {0} image URL's and {1} album URL's from {2} pages", imageUrls.Count, albumUrls.Count, pageCount);
 
             // Parse out image URL's from the albums
             var imagesFromAlbums = albumUrls.SelectMany(albumUrl => ImgurUtils.ParseAlbum(new Uri(albumUrl)));
             imageUrls.UnionWith(imagesFromAlbums);
             Console.WriteLine("There are {0} total images after extracting {1} from albums", imageUrls.Count, imagesFromAlbums.ToList().Count);
 
-            // TODO:  Populate the data grid and other fields on the second tab
+            PageCountLabel.Content = pageCount;
+            ImagesCountLabel.Content = imageUrls.Count;
+            ImagesDataGrid.ItemsSource = imageUrls.Select(imageUrl => new ImgurUrl { URL = imageUrl });
         }
+
+        /// <summary>
+        /// Event handler to add row numbers to the main data grid when its contents are updated.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ImagesDataGrid_LoadingRow(object sender, DataGridRowEventArgs e)
+        {
+            e.Row.Header = (e.Row.GetIndex() + 1).ToString();
+        }
+    }
+
+    /// <summary>
+    /// Inner class to store image URL strings, for use by the main data grid.
+    /// </summary>
+    public class ImgurUrl
+    {
+        public String URL { get; set; }
     }
 }
